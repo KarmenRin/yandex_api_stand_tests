3dfa20df022cbe482f94551b121290d2bc22d51c
Irina <karmenrin@list.ru>
2023-06-27 14:41:42 +0600

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://85733ad8-a872-44d9-ac0f-407e900d5d73.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
